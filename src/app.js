4359f9d8cd3d7abeeeb40abd54da54d5a4b1be80
nauticalstars <nauticalstars21@gmail.com>
2023-02-28 21:22:11 -0800

    Created live date and time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,18 @@
+function formateDate(timeStamp) {
+  let date = new Date(timeStamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let day = days[date.getDay()];
+  return `${day} ${hours} : ${minutes}`;
+}
+
 function displayTemp(response) {
   console.log(response.data);
 
@@ -7,6 +22,7 @@ function displayTemp(response) {
   let precipitationElemet = document.querySelector("#feelsLike");
   let humidityElement = document.querySelector("#Humidity");
   let windElement = document.querySelector("#Wind");
+  let dateElemet = document.querySelector("#date");
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   cityElement.innerHTML = response.data.city;
   conditionElement.innerHTML = response.data.condition.description;
@@ -15,6 +31,7 @@ function displayTemp(response) {
   );
   humidityElement.innerHTML = response.data.temperature.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElemet.innerHTML = formateDate(response.data.time * 1000);
 }
 let apiKey = "89045e8b02ffo7bc061tb52f38ead08c";
 let units = "metric";

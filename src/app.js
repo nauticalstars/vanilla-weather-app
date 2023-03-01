5bf1627b86d2434b072c489c4e98e15c3c6480d8
nauticalstars <nauticalstars21@gmail.com>
2023-02-28 21:39:37 -0800

    Live weather icon and alt attribute

--- src/app.js
+++ src/app.js
@@ -14,8 +14,6 @@ function formateDate(timeStamp) {
 }
 
 function displayTemp(response) {
-  console.log(response.data);
-
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let conditionElement = document.querySelector("#condition");
@@ -23,6 +21,7 @@ function displayTemp(response) {
   let humidityElement = document.querySelector("#Humidity");
   let windElement = document.querySelector("#Wind");
   let dateElemet = document.querySelector("#date");
+  let currentIcon = document.querySelector("#icon");
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   cityElement.innerHTML = response.data.city;
   conditionElement.innerHTML = response.data.condition.description;
@@ -32,9 +31,11 @@ function displayTemp(response) {
   humidityElement.innerHTML = response.data.temperature.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElemet.innerHTML = formateDate(response.data.time * 1000);
+  currentIcon.setAttribute("src", response.data.condition.icon_url);
+  currentIcon.setAttribute("alt", response.data.condition.icon);
 }
 let apiKey = "89045e8b02ffo7bc061tb52f38ead08c";
 let units = "metric";
-let city = "Paris";
+let city = "Whistler";
 let Url = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=${units}`;
 axios.get(Url).then(displayTemp);
